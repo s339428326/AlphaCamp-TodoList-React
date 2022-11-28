54137d0566d392d247a4b2806b217b50b89372de
翁柏豐 <s339428326@gmail.com>
2022-11-28 10:41:25 +0800

    feat:test commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
 function App() {
   return (
     <div className="app">
+      <TodoPage />
       <BrowserRouter>
         <Routes>
-          <Route path="*" element={<HomePage />} />
+          {/* <Route path="*" element={<HomePage />} /> */}
           <Route path="signup" element={<SignUpPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="todo" element={<TodoPage />} />
